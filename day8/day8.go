8100489a660c043bf51920d4f728cf2ed3605de6
szep <szepiet33@gmail.com>
2026-03-05 22:03:35 +0100

    start day 8

--- day8/day8.go
+++ day8/day8.go
@@ -1,1 +1,14 @@
 package day8
+
+type Point struct {
+	x int
+	y int
+}
+
+func SolveFirstPart(lines []string) int64 {
+	return 0
+}
+
+func SolveSecondPart(lines []string) int64 {
+	return 0
+}
--- day8/day8_test.go
+++ day8/day8_test.go
@@ -1,1 +1,30 @@
 package day8
+
+import (
+	"AoC2025/utils"
+	"testing"
+)
+
+func TestFirstPart(t *testing.T) {
+	lines, err := utils.LoadFileAsLines("example.txt")
+	if err != nil {
+		t.Fatalf("Failed to load example file: %v", err)
+	}
+	expected := int64(0)
+	result := SolveFirstPart(lines)
+	if result != expected {
+		t.Errorf("SolveFirstPart() = %v; want %v", result, expected)
+	}
+}
+
+func TestSecondPart(t *testing.T) {
+	lines, err := utils.LoadFileAsLines("example.txt")
+	if err != nil {
+		t.Fatalf("Failed to load example file: %v", err)
+	}
+	expected := int64(0)
+	result := SolveSecondPart(lines)
+	if result != expected {
+		t.Errorf("SolveSecondPart() = %v; want %v", result, expected)
+	}
+}
