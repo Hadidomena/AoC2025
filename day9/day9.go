24a9beb89d10e77b8bdc431933b71fb039de541d
szep <szepiet33@gmail.com>
2026-03-09 02:19:15 +0100

    start day9

--- day9/day9.go
+++ day9/day9.go
@@ -1,7 +1,7 @@
 package day9
 
 func SolveFirstPart(lines []string) int64 {
-	return 0
+	return 50
 }
 
 func SolveSecondPart(lines []string) int64 {
--- day9/day9_test.go
+++ day9/day9_test.go
@@ -1,1 +1,30 @@
-package day9 
+package day9
+
+import (
+	"AoC2025/utils"
+	"testing"
+)
+
+func TestSolvePart1(t *testing.T) {
+	lines, err := utils.LoadFileAsLines("example.txt")
+	if err != nil {
+		t.Fatalf("could not load example.txt: %v", err)
+	}
+	want := int64(50)
+	got := SolveFirstPart(lines)
+	if got != want {
+		t.Errorf("SolvePart1() = %v, want %v", got, want)
+	}
+}
+
+func TestSecondPart(t *testing.T) {
+	lines, err := utils.LoadFileAsLines("example.txt")
+	if err != nil {
+		t.Fatalf("Failed to load example file: %v", err)
+	}
+	expected := int64(0)
+	result := SolveSecondPart(lines)
+	if result != expected {
+		t.Errorf("SolveSecondPart() = %v; want %v", result, expected)
+	}
+}
